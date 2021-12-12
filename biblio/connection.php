223e2594accd9e3edbca0738a61424abb7f7e6ff
MMaue <73600075+MMaue@users.noreply.github.com>
2021-12-12 20:48:52 +0100

    Update connection.php

--- biblio/connection.php
+++ biblio/connection.php
@@ -17,10 +17,11 @@ $dbname = "bibliothekssystem";
 
 $con = new mysqli($dbhost,$dbuser,$dbpass,$dbname);
 if($con->connect_error) {
-	echo "<img src=\"red_x_wiki.svg\" alt=\"NOT Connected\"  width=\"25px\" height=\"25px\"/> NOT connected";
+	//echo "<img src=\"red_x_wiki.svg\" alt=\"NOT Connected\"  width=\"25px\" height=\"25px\"/> NOT connected";
 	//die("failed to connect<br>".$con->connect_error);
 }
 else {
-	echo "<img src=\"green_tick_wiki.svg\" alt=\"Connected\"  width=\"25px\" height=\"25px\"/> connected";
+	//echo "<img src=\"green_tick_wiki.svg\" alt=\"Connected\"  width=\"25px\" height=\"25px\"/> connected";
 }
+//! echo can NOT be used in here because it is executed before the header function
 ?>
